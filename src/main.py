5a77219d27ed79dc941c1cbcaea21dc1e1c5c96d
Rapinsa <Rapinsaarzyka16@gmail.com>
2026-06-06 19:16:03 +0700

    update to the src/main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
-print('halo dunia')
+from rich.console import Console
+from rich.panel import Panel
+import psutil
+
+console = Console()
+
+cpu = psutil.cpu_percent(interval=1)
+ram = psutil.virtual_memory
+disk = psutil.disk_usage('/')
+
+console.print(
+    Panel.fit(
+        f"[bold cyan]RAR Labs[/bold cyan]\n\nCPU: {cpu}%\nRAM {ram}%\nDISK {disk}%",
+        title="RAR Dynamics"
+    )
+)
