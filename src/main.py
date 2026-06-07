27b898d802f983099e76470b8d3645716dd26cdc
Rapinsa <Rapinsaarzyka16@gmail.com>
2026-06-07 07:44:41 +0700

    fixing the code a little bit

--- src/main.py
+++ src/main.py
@@ -1,16 +1,29 @@
-from rich.console import Console
+from rich.console import Console, Group
 from rich.panel import Panel
+from rich.text import Text
 import psutil
 
 console = Console()
 
 cpu = psutil.cpu_percent(interval=1)
-ram = psutil.virtual_memory
+ram = psutil.virtual_memory()
 disk = psutil.disk_usage('/')
+net = psutil.net_io_counters()
+
+title = Text("RAR Labs", style="bold cyan")
+
+content = Group(
+    title,
+    Text(""),
+    Text(f"CPU: {cpu}%"),
+    Text(f"RAM: {ram.percent}%"),
+    Text(f"DISK: {disk.percent}%"),
+    Text(f"NET: {net.bytes_recv}%"),
+) 
 
 console.print(
     Panel.fit(
-        f"[bold cyan]RAR Labs[/bold cyan]\n\nCPU: {cpu}%\nRAM {ram}%\nDISK {disk}%",
+        content,
         title="RAR Dynamics"
     )
 )
