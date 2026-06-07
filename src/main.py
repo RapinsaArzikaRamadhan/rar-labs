2bb214180db533b7173c504dd0945b853de5350d
Rapinsa <Rapinsaarzyka16@gmail.com>
2026-06-07 10:59:32 +0700

    making a good layout using rich panel and rich live

--- src/main.py
+++ src/main.py
@@ -1,29 +1,57 @@
 from rich.console import Console, Group
 from rich.panel import Panel
 from rich.text import Text
+from rich.layout import Layout
+from rich.live import Live
+import time
 import psutil
 
 console = Console()
+layout = Layout()
 
-cpu = psutil.cpu_percent(interval=1)
-ram = psutil.virtual_memory()
-disk = psutil.disk_usage('/')
-net = psutil.net_io_counters()
+layout.split_column(
+    Layout(name="header", size=3),
+    Layout(name="body"),
+    Layout(name="logs", size=8)
+)
+
+layout["body"].split_row(
+    Layout(name="left"),
+    Layout(name="right")
+)
 
 title = Text("RAR Labs", style="bold cyan")
 
-content = Group(
-    title,
-    Text(""),
-    Text(f"CPU: {cpu}%"),
-    Text(f"RAM: {ram.percent}%"),
-    Text(f"DISK: {disk.percent}%"),
-    Text(f"NET: {net.bytes_recv}%"),
-) 
+with Live(layout, refresh_per_second=2, screen=True) as live:
+    
+    while True:
+        cpu = psutil.cpu_percent(interval=None)
+        ram = psutil.virtual_memory()
+        disk = psutil.disk_usage('/')
+        net = psutil.net_io_counters()
+        width = console.size.width
+        height = console.size.height
 
-console.print(
-    Panel.fit(
-        content,
-        title="RAR Dynamics"
-    )
-)
+        layout["header"].update(
+            Panel(
+                f"{title}"
+                )
+        )
+        layout["left"].update(
+            Panel(
+                f"CPU:{cpu}%\nDISK:{disk.percent}%"
+                )
+        )
+        layout["right"].update(
+            Panel(
+                f"RAM: {ram.percent}%\nNET: {net.bytes_recv / 1024**3:.2f}GB"
+                )
+        )
+
+        layout["logs"].update(
+            Panel(
+                f"upcoming"
+                )
+        )
+
+        time.sleep(1)
