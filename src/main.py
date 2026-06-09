333ed85468a79c01140227dbbcdffcb28dc4f4f8
Rapinsa <Rapinsaarzyka16@gmail.com>
2026-06-09 10:03:50 +0700

    idk i adding some new bar to the net speed

--- src/main.py
+++ src/main.py
@@ -22,6 +22,25 @@ def bar(percent):
     filled = int(percent / 5)
     return "▰" * filled + "▱" * (20 - filled)
 
+def net_bar(speed, max_speed=1024*1024):
+    percent = min(speed  / max_speed * 100, 100)
+    return bar(percent)
+# riwayat_ram = []
+
+# def gfk(data):
+#     karakter = "⣀⣄⣤⣦⣶⣷⣿"
+#     hasil = ""
+
+#     for nilai in data:
+#         indeks = int(
+#             nilai / 100 * (len(karakter)  -1)
+#         )
+    
+#         hasil += karakter[indeks]
+
+#     return hasil
+
+old = psutil.net_io_counters()
 
 with Live(layout, refresh_per_second=2, screen=True) as live:
     while True:
@@ -40,19 +59,48 @@ with Live(layout, refresh_per_second=2, screen=True) as live:
         C1t = temps["coretemp"][1].current
 
         ram = psutil.virtual_memory()
+        # riwayat_ram.append(ram.percent)
+        # if len(riwayat_ram) > 20:
+        #     riwayat_ram.pop(0)
+
         disk = psutil.disk_usage("/")
         net = psutil.net_io_counters()
+        new = psutil.net_io_counters()
+        old = new
+
+        recv = new.bytes_recv - old.bytes_recv
+        sent = new.bytes_sent - old.bytes_sent
+
         width = console.size.width
         height = console.size.height
 
         layout["header"].update(Panel(f"{title}"))
         layout["left"].update(
             Panel(
-                f"CPU:{cpu}%\nC0:{bar(C0)} {C0:.1f}% CT{bar(C0t)} {C0t:.1f}%\nC1:{bar(C1)} {C1:.1f}% CT{bar(C1t)} {C1t:.1f}%\nC2:{bar(C2)} {C2:.1f}%\nC3:{bar(C3)} {C3:.1f}%\n\nDISK:{disk.percent}%"
+                Group(
+                    Text(f"CPU:{cpu}%"),
+                    Text(f"C0:{bar(C0)} {C0:.1f}% CT{bar(C0t)} {C0t:.1f}%"),
+                    Text(f"C1:{bar(C1)} {C1:.1f}% CT{bar(C1t)} {C1t:.1f}%"),
+                    Text(f"C2:{bar(C2)} {C2:.1f}%"),
+                    Text(f"C3:{bar(C3)} {C3:.1f}%"),
+                    Text(""),
+                    Text(f"DS:{bar(disk.percent)} {disk.percent}%"),
+                ),
+                title="LEFT PANEL",
             )
         )
         layout["right"].update(
-            Panel(f"RAM: {ram.percent}%\n\nNET: {net.bytes_recv / 1024**3:.2f}GB")
+            Panel(
+                Group(
+                    # Text(f"RAM: {gfk(riwayat_ram)} {ram.percent:.1f}%"),
+                    # Text(f"NET: {net.bytes_recv / 1024**3:.2f}GB"),
+                    Text(f"RAM: {bar(ram.percent)} {ram.percent:.1f}%"),
+                    Text(f"NET: {net_bar(sent)} {sent/1024:.1f}KB/s"),
+                    Text(f"NET: {net_bar(recv)} {recv/1024:.1f}KB/s"),
+                    Text(f"DEBUG recv={recv} sent={sent}")
+                ),
+                title="RIGHT PANEL",
+            )
         )
 
         layout["logs"].update(Panel(f"upcoming"))
